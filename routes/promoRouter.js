4b6c9a91163692d93c430c1c9b835bdd41674a68
Pavel Cikiliov <pavelchi91@gmail.com>
2020-08-24 15:24:57 +0300

    User Authentication with Passport and JSON Web Token

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+var authenticate = require('../authenticate');
 const Promotions = require("../models/promotions");
 
 const promoRouter = express.Router();
@@ -19,7 +19,7 @@ promoRouter.route("/")
         },(err) => next(err))
       .catch((err) => next(err));
   })
-  .post((req, res, next) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     Promotions.create(req.body)
       .then(
         (promotion) => {
@@ -30,11 +30,11 @@ promoRouter.route("/")
         },(err) => next(err))
       .catch((err) => next(err));
   })
-  .put((req, res, next) => {
+  .put(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end("PUT operation not supported on /promotions");
   })
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Promotions.remove({})
       .then(
         (resp) => {
@@ -61,11 +61,11 @@ promoRouter
       )
       .catch((err) => next(err));
   })
-  .post((req, res, next) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end("POST operation not supported on /promotion/" + req.params.promoId);
   })
-  .put((req, res, next) => {
+  .put(authenticate.verifyUser, (req, res, next) => {
     Promotions.findByIdAndUpdate(
       req.params.promoId,
       {
@@ -83,7 +83,7 @@ promoRouter
       )
       .catch((err) => next(err));
   })
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Promotions.findByIdAndRemove(req.params.promoId)
       .then(
         (resp) => {
